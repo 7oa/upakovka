9f8080dab5235831fa85a9229fe1cbb1f85c9f9c
7oa <semenova.7oa@yandex.ru>
2017-09-06 15:52:04 +0300

    new pages

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -3,17 +3,16 @@ $(document).ready(function() {
 	var galleryTop = new Swiper('.top-slider__full', {
 		effect: 'fade'
 	});
-	/*var galleryThumbs = new Swiper('.top-slider__thumbs', {
-		centeredSlides: false,
-		slidesPerView: 'auto',
-		touchRatio: 0.2,
-		slideToClickedSlide: true
-	});
-	galleryTop.params.control = galleryThumbs;
-	galleryThumbs.params.control = galleryTop;*/
 	$(".top-slider__thumbs").on('click', '.top-slider__prev', function(){
 		galleryTop.slideTo($(this).index(), 500);
 		$('.top-slider__prev').removeClass('active');
 		$(this).addClass('active');
+	});
+
+	var swiper = new Swiper('.partners__slider', {
+		nextButton: '.swiper-next',
+		prevButton: '.swiper-prev',
+		slidesPerView: 5,
+		spaceBetween: 60
 	});
 });
