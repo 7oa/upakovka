534999ddfb268239fb43c414e0e01cf9bf0b0b16
7oa <semenova.7oa@yandex.ru>
2017-09-11 13:32:25 +0300

    final

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,6 +2,7 @@
 //= plugins/_jquery-3.2.0.min.js
 //= plugins/_swiper.min.js
 //= plugins/_jquery.maskedinput.min.js
+//= plugins/_jquery.fancybox.min.js
 
 //Scripts
 //= scripts/script.js
--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -16,6 +16,22 @@ $(document).ready(function() {
 		spaceBetween: 60
 	});
 
+	var swiper = new Swiper('.viewed__body', {
+		nextButton: '.viewed__next',
+		prevButton: '.viewed__prev',
+		slidesPerView: 6,
+		spaceBetween: 30
+	});
+
+	var swiper = new Swiper('.detail__gallery', {
+		nextButton: '.detail-next',
+		prevButton: '.detail-prev',
+		spaceBetween: 10,
+		autoHeight: true,
+		slidesPerView: 'auto',
+		direction: 'vertical'
+	});
+
 	$(".js-tab").click(function(){
 		var tab = $(this).data("tab");
 		$(".js-tab.selected").removeClass("selected");
@@ -25,4 +41,12 @@ $(document).ready(function() {
 	});
 
 	$("#phone").mask("+7 (999) 999-99-99", {placeholder: "+7 (___) ___-__-__" });
+
+	$('.js-prev').click(function () {
+		$('.js-prev').removeClass('active');
+		$(this).addClass('active');
+		var bg =  $(this).css('background-image');
+		console.log(bg);
+		$('.js-big').css('background-image',bg);
+	});
 });
